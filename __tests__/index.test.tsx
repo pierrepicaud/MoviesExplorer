9275dd29b7a1a2c707c4ca9cc06cb3a0af83f9dd
Son Truong Nguyen <t.nguyen@innopolis.university>
2022-11-20 17:04:54 +0300

    movie grid test added

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,7 +1,6 @@
 import {cleanup, render, screen} from '@testing-library/react'
 import Home from '../pages'
 import '@testing-library/jest-dom'
-import MovieCard from '../components/MovieCard'
 
 afterEach(() => {
     cleanup()
